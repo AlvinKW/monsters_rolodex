c9480dd7b099e00b4d73963c014293ceb6d04cc4
Alvin Kwan <alvink88@bu.edu>
2021-04-08 14:37:18 -0400

    added more features

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello Alvin'
-    }
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  //life cycle method to fetch data from url and converting into json format
+  //take users and set monsters to that array 
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           {this.state.string}
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello Alvin Kwan '})}>
-          Change Text
-          </button>
-        </header>
+      <input 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={e => this.setState({ searchField: e.target.value })}
+      />
+      <CardList monsters={this.state.monsters}/>
       </div>
     );
   }
